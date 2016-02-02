efdea4cdeca4948f0151acc09ed2c04c34590524
paulbardar <q.c.tulip@gmail.com>
2016-02-02 21:19:17 +0200

    fluid menu & js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 ;(function(){
+	var menuToggle = document.getElementById("ba-menu-toggle"),
+		body = document.body;
 
+	menuToggle.onclick = function(event){
+		event.preventDefault();
+		if(body.className === "ba-menu-opened"){
+			body.className = "";
+		} else {
+			body.className = "ba-menu-opened";
+		}
+
+	}
 })();
